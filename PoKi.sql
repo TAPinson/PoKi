f75ba70cb6c8b11ba9c7868c7e56417b97122a93
Travis Pinson <travispinson@gmail.com>
2020-12-01 13:42:25 -0600

    #11 Complete

--- PoKi.sql
+++ PoKi.sql
@@ -1,27 +1,27 @@
-﻿--1
+﻿--1 What grades are stored in the database?
 --SELECT Name
 --FROM Grade
 
---2
+--2 What emotions may be associated with a poem?
 --SELECT Name
 --FROM Emotion
 
---3
+--3 How many poems are in the database?
 --SELECT COUNT(Poem.Id)
 --FROM Poem
 
---4
+--4 Sort authors alphabetically by name. What are the names of the top 76 authors?
 --SELECT TOP 76 Author.Name
 --FROM Author
 --ORDER BY Author.Name
 
---5
+--5 Starting with the above query, add the grade of each of the authors.
 --SELECT TOP 76 Author.Name, Grade.Name
 --FROM Author
 --JOIN Grade ON Author.GradeId = Grade.Id
 --ORDER BY Author.Name, Grade.Name
 
---6
+--6 Starting with the above query, add the recorded gender of each of the authors.
 --SELECT TOP 76 Author.Name, Grade.Name AS GradeName, Gender.Name AS GenderName
 --FROM Author
 --JOIN Grade ON Author.GradeId = Grade.Id
@@ -29,23 +29,29 @@
 --ORDER BY Author.Name, Grade.Name, Gender.Name
 
 
---7
+--7 What is the total number of words in all poems in the database?
 --SELECT SUM(Poem.WordCount)
 --FROM Poem
 
---8
+--8 Which poem has the fewest characters?
 --SELECT TOP 1 Poem.Title, Poem.CharCount
 --FROM Poem
 --ORDER BY Poem.CharCount, Poem.Title
 
---9 Not 100% sure if this is correct, but I think so
+--9 How many authors are in the third grade?
 --SELECT COUNT(Author.GradeId)
 --FROM Author
 --WHERE Author.GradeId = 3
 
---10
+--10 How many authors are in the first, second or third grades?
 --SELECT GradeId, COUNT(GradeId) AS Authors
 --FROM Author
 --WHERE GradeId = 1 OR GradeId = 2 OR GradeId = 3
 --GROUP BY GradeId
 --ORDER BY GradeId
+
+--11 What is the total number of poems written by fourth graders?
+--SELECT COUNT(Poem.Id) AS NumOfPoems
+--FROM Poem
+--JOIN Author ON Author.Id = Poem.AuthorId
+--WHERE Author.GradeId = 4
